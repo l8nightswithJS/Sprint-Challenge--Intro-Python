dd3df27a77890b4458cb41f7456f252631781b49
Eddie Jimenez <52768061+l8nightswithJS@users.noreply.github.com>
2020-09-11 20:56:05 -0700

    not done lost track of time got stuck

--- src/comp/comp.py
+++ src/comp/comp.py
@@ -24,7 +24,7 @@ humans = [
 # Write a list comprehension that creates a list of names of everyone
 # whose name starts with 'D':
 print("Starts with D:")
-a = []
+a["name"] = [x.split()[1][0] == "D" for x in humans.Human["D"]]
 print(a)
 
 # Write a list comprehension that creates a list of names of everyone
--- src/oop/oop1.py
+++ src/oop/oop1.py
@@ -17,3 +17,21 @@
 #     pass
 #
 # Put a comment noting which class is the base class
+class Vehicle_Classifications:
+    def __init__(self, name):
+        self.name = name
+
+    def says_class(self):
+        print("This is a ", self.name)
+
+class Vehicle_Types(Vehicle_Classifications):
+    def __init__(self, v_type):
+        self.v_type = v_type
+
+    def says_types(self):
+        print("This" + Vehicle_Classifications + "it is a", self.v_type)
+
+y = Vehicle_Types("Ground-Vehicle")
+x = Vehicle_Classifications("Vehicle")
+y.says_types()
+        
